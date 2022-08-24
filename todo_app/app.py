a7544d010c6250d2af4e7809de2d3f9e2b90a2c3
Jackie Woodburn <jackie.woodburn@softwire.com>
2022-08-24 17:09:31 +0100

    exercise-2: Added ability to move item between lists

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,12 +1,15 @@
+from calendar import c
 from flask import Flask, redirect, render_template, request
 from todo_app.data.session_items import add_item, get_items
-from todo_app.data.trello_items import create_item_on_todo_list, get_lists_on_board
+from todo_app.data.trello_items import create_item_on_todo_list, get_lists_on_board, update_item_list_id
 
 from todo_app.flask_config import Config
 from todo_app.item import Item
 
 app = Flask(__name__)
 app.config.from_object(Config())
+
+list_ids_in_progression_order = []
 
 @app.route('/')
 def index():
@@ -16,9 +19,11 @@ def index():
 
     for list in lists_on_board:
         list_names.append(list['name'])
+        list_ids_in_progression_order.append(list['id'])
         for card in list['cards']:
             items_on_board.append(Item.from_trello_card(card, list))
 
+    print(list_ids_in_progression_order)
     return render_template('index.html', column_names=list_names, todos=items_on_board)
 
 @app.route('/add-todo', methods=[ 'POST'])
@@ -27,4 +32,18 @@ def add_todo_item():
     # TODO exercise-2: hard coding this to add to start of flow (To Do) for now
     create_item_on_todo_list(new_item)
     return redirect('/')
-    
+
+@app.route('/update-item-status/<item_id>/<current_list_id>', methods=[ 'POST'])
+def update_item_status(item_id, current_list_id):
+    new_list_id = _get_next_list_id(current_list_id)
+    print(current_list_id, new_list_id)
+    update_item_list_id(item_id, new_list_id)
+    return redirect('/')
+
+def _get_next_list_id(current_list_id):
+    current_index = list_ids_in_progression_order.index(current_list_id)
+    next_index = current_index + 1
+    if next_index > (len(list_ids_in_progression_order) - 1):
+        # TODO exercise-2: Should probably throw an error here, they shouldn't be able to click next on a Done item
+        return current_list_id
+    return list_ids_in_progression_order[next_index]
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -8,6 +8,7 @@ board_id = os.getenv('TRELLO_BOARD_ID')
 
 # TODO exercise-2: is there a nice way to not hard code
 todo_list_id = '6305fd4bf77620537efc5cf5'
+doing_list_id = '63063ebcfa6c6b7b70deac42'
 done_list_id = '6305fd4c6de4d31d4d1a97b3'
 
 def get_lists_on_board():
@@ -30,4 +31,13 @@ def create_item_on_list(item_name, list_id):
         'name': item_name,
         'idList': list_id
     }
-    requests.post(create_item_endpoint, params=params).json()
+    requests.post(create_item_endpoint, params=params)
+
+def update_item_list_id(item_id, updated_list_id):
+    update_item_endpoint = f'{trello_base_url}cards/{item_id}'
+    params = {
+        'key': api_key,
+        'token': api_token,
+        'idList': updated_list_id
+    }
+    requests.put(update_item_endpoint, params=params)
