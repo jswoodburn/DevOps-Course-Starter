6c88d03c075627f693b7788609cb52757498d3df
Jackie Woodburn <jackie.woodburn@softwire.com>
2022-09-23 10:27:58 +0100

    exericse-3: Updated view model filter methods to pass tests

--- todo_app/view_model.py
+++ todo_app/view_model.py
@@ -10,3 +10,27 @@ class ViewModel:
     @property
     def column_names(self):
         return self._column_names
+
+    @property
+    def todo_items(self):
+        todo_items = []
+        for item in self._items:
+            if item.status == 'To Do':
+                todo_items.append(item)
+        return todo_items
+
+    @property
+    def doing_items(self):
+        doing_items = []
+        for item in self._items:
+            if item.status == 'Doing':
+                doing_items.append(item)
+        return doing_items
+
+    @property
+    def done_items(self):
+        done_items = []
+        for item in self._items:
+            if item.status == 'Done':
+                done_items.append(item)
+        return done_items
