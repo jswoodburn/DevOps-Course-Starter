8836cdbb11bd875b82f76deb796fccb2fe610fab
Jackie Woodburn <jackie.woodburn@softwire.com>
2022-09-23 11:50:39 +0100

    exercise-3: Templated integration tests and mocked trello api response

---
+++ tests/test_app.py
@@ -0,0 +1,51 @@
+import os
+import requests
+import pytest
+from dotenv import load_dotenv, find_dotenv
+from todo_app import app
+
+@pytest.fixture
+def client():
+    file_path = find_dotenv('.env.test')
+    load_dotenv(file_path, override=True)
+
+    test_app = app.create_app()
+
+    with test_app.test_client() as client:
+        yield client
+
+def test_index_page(monkeypatch, client):
+    monkeypatch.setattr(requests, 'get', get_lists_stub)
+    response = client.get('/')
+
+class StubResponse():
+    def __init__(self, fake_response_data):
+        self.fake_response_data = fake_response_data
+
+    def json(self):
+        return self.fake_response_data
+
+def get_lists_stub(url, params):
+    test_board_id = os.environ.get('TRELLO_BOARD_ID')
+
+    print(url)
+    # TODO exercise-3: Get this working with environment variables (currently test_board_id returns None)
+    # fake_response_data = None
+    # if url == f'https://api.trello.com/1/boards/{test_board_id}/lists':
+    fake_response_data = [
+        {
+            'idBoard': '1234ABCD',
+            'id': '890xyz',
+            'name': 'To Do',
+            'cards': [
+                {
+                    'id': '456', 
+                    'name': 'Test card', 
+                    'dateLastActivity': '2022-09-06T15:45:55.959Z',
+                    'idBoard': '1234ABCD',
+                    'idList': '890xyz',
+                },
+            ],
+        }
+    ]
+    return StubResponse(fake_response_data)
--- todo_app/app.py
+++ todo_app/app.py
@@ -8,50 +8,52 @@ from todo_app.flask_config import Config
 from todo_app.item import Item
 from todo_app.view_model import ViewModel
 
-app = Flask(__name__)
-app.config.from_object(Config())
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
-list_ids_in_progression_order = []
+    list_ids_in_progression_order = []
 
-@app.route('/')
-def index():
-    lists_on_board = get_lists_on_board()
-    list_names = []
-    items_on_board = []
+    @app.route('/')
+    def index():
+        lists_on_board = get_lists_on_board()
+        list_names = []
+        items_on_board = []
 
-    for list in lists_on_board:
-        list_names.append(list['name'])
-        list_ids_in_progression_order.append(list['id'])
-        for card in list['cards']:
-            if not was_card_completed_before_today(list, card):
-                items_on_board.append(Item.from_trello_card(card, list))
+        for list in lists_on_board:
+            list_names.append(list['name'])
+            list_ids_in_progression_order.append(list['id'])
+            for card in list['cards']:
+                if not was_card_completed_before_today(list, card):
+                    items_on_board.append(Item.from_trello_card(card, list))
 
-    return render_template('index.html', view_model=ViewModel(items_on_board, list_names))
+        return render_template('index.html', view_model=ViewModel(items_on_board, list_names))
 
-def get_date_time_from_string(dateAsString):
-    return datetime.strptime(dateAsString, "%Y-%m-%dT%H:%M:%S.%fZ").date()
+    def get_date_time_from_string(dateAsString):
+        return datetime.strptime(dateAsString, "%Y-%m-%dT%H:%M:%S.%fZ").date()
 
-def was_card_completed_before_today(list, card):
-    return list['name'] == 'Done' and get_date_time_from_string(card['dateLastActivity']) != datetime.today().date()
+    # TODO exercise-3: Did this before reading stretch goal. Come back and do this with TDD and view model if time.
+    def was_card_completed_before_today(list, card):
+        return list['name'] == 'Done' and get_date_time_from_string(card['dateLastActivity']) != datetime.today().date()
 
-@app.route('/add-todo', methods=[ 'POST'])
-def add_todo_item():
-    new_item = request.form.get('todo')
-    # TODO exercise-2: hard coding this to add to start of flow (To Do) for now
-    create_item_on_todo_list(new_item)
-    return redirect('/')
+    @app.route('/add-todo', methods=[ 'POST'])
+    def add_todo_item():
+        new_item = request.form.get('todo')
+        create_item_on_todo_list(new_item)
+        return redirect('/')
 
-@app.route('/update-item-status/<item_id>/<current_list_id>', methods=[ 'POST'])
-def update_item_status(item_id, current_list_id):
-    new_list_id = _get_next_list_id(current_list_id)
-    print(current_list_id, new_list_id)
-    update_item_list_id(item_id, new_list_id)
-    return redirect('/')
+    @app.route('/update-item-status/<item_id>/<current_list_id>', methods=[ 'POST'])
+    def update_item_status(item_id, current_list_id):
+        new_list_id = _get_next_list_id(current_list_id)
+        update_item_list_id(item_id, new_list_id)
+        return redirect('/')
 
-def _get_next_list_id(current_list_id):
-    current_index = list_ids_in_progression_order.index(current_list_id)
-    next_index = current_index + 1
-    if next_index > (len(list_ids_in_progression_order) - 1):
-        # TODO exercise-2: Should probably throw an error here, they shouldn't be able to click next on a Done item
-        return current_list_id
-    return list_ids_in_progression_order[next_index]
+    def _get_next_list_id(current_list_id):
+        current_index = list_ids_in_progression_order.index(current_list_id)
+        next_index = current_index + 1
+        if next_index > (len(list_ids_in_progression_order) - 1):
+            # TODO exercise-2: Should probably throw an error here, they shouldn't be able to click next on a Done item
+            return current_list_id
+        return list_ids_in_progression_order[next_index]
+
+    return app
