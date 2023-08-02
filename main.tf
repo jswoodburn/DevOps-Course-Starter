c8d7e8375ebe9ae79124a204b150b1e13d1a10fd
Jackie Woodburn <jackie.woodburn@softwire.com>
2023-08-02 16:39:06 +0100

    exercise-12: set up db in terraform

--- main.tf
+++ main.tf
@@ -31,12 +31,57 @@ resource "azurerm_linux_web_app" "main" {
 
   site_config {
     application_stack {
-      docker_image     = "appsvcsample/python-helloworld"
-      docker_image_tag = "latest"
+      docker_image     = "jackiew104/todo-app"
+      docker_image_tag = "prod"
     }
   }
 
   app_settings = {
     "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
+    "FLASK_APP" = "todo_app/app"
+    "FLASK_ENV" = "development"
+    "SECRET_KEY" = "SECRET_KEY"
+    "COSMOS_CONNECTION_STRING" = azurerm_cosmosdb_account.main.connection_strings[0]
+    "COSMOS_DB_NAME" = azurerm_cosmosdb_mongo_database.main.name
   }
+}
+
+resource "azurerm_cosmosdb_account" "main" {
+  name                = "terraformed-jsw-todo-app"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+  
+  lifecycle { 
+    prevent_destroy = true 
+  }
+
+  capabilities {
+    name = "EnableServerless"
+  }
+
+# TODO exercise-12: not sure if this is right
+  consistency_policy {
+    consistency_level = "Strong"
+  }
+
+  geo_location {
+    location          = "uksouth"
+    failover_priority = 0
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
+}
+
+resource "azurerm_cosmosdb_mongo_database" "main" {
+  name                = "terraformed-dev-db"  # TODO exercise-12: make this rely on env variables
+  resource_group_name = data.azurerm_resource_group.main.name
+  account_name        = azurerm_cosmosdb_account.main.name
 }
